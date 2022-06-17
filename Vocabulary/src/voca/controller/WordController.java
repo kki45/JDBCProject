05d322169aae8456b29a4c2116c055716283d49e
jomye <jomye@LAPTOP-1OIGKSKB>
2022-06-17 13:31:36 +0900

    예외 처리 작업

--- Vocabulary/src/voca/controller/WordController.java
+++ Vocabulary/src/voca/controller/WordController.java
@@ -1,9 +1,13 @@
 package voca.controller;
 
+import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import voca.dto.WordFormVO;
+import voca.exception.ValidationException;
 import voca.service.WordService;
 import voca.view.RunningEndView;
-
 
 public class WordController {
 	private static WordController instance = new WordController();
@@ -17,7 +21,7 @@ public class WordController {
 		return instance;
 	}
 
-	//모든 단어 출력
+	// 모든 단어 출력
 	public void allVoca() {
 
 		try {
@@ -27,65 +31,73 @@ public class WordController {
 		}
 
 	}
-	
+
 	// 힌 단어 출력
-	
-	public void searchVoca(String koreanWord) {
+
+	public boolean searchVoca(String koreanWord) throws SQLException, ValidationException  {
+		Pattern pattern = Pattern.compile("^[0-9]*$");
+		Matcher matcher = pattern.matcher(koreanWord);
 		try {
-			RunningEndView.searchVoca(service.searchVoca(koreanWord));
-		} catch (Exception e) {
-			if(e.toString().equals("java.lang.NullPointerException")) {
-				System.out.println("검색 결과가 존재 하지 않습니다.");
+			if(matcher.find() == true) {
+				ValidationException exception = new ValidationException("숫자는 입력 하실 수 없습니다.");
 			}
+//			
+			RunningEndView.searchVoca(service.searchVoca(koreanWord));
+			return true;
+		} catch (NullPointerException n) {
+			System.out.println("검색 결과가 존재 하지 않습니다.");
 		}
+		return false;
+
 	}
-	
+
 	// 영어단어 추가
-	
+
 	public void addVoca(WordFormVO wordFormVO) {
 		try {
-			boolean addVoca = service.addVoca(wordFormVO);
-			if(addVoca == true) {
-				System.out.println("단어가 추가 되었습니다.");
+			boolean searchVoca = searchVoca(wordFormVO.getKoreanWord());
+			if(searchVoca == true) {
+				System.out.println("단어가 중복 됩니다.");
+			}else {
+				boolean addVoca = service.addVoca(wordFormVO);
+				if (addVoca == true) {
+					System.out.println("단어가 추가 되었습니다.");
+				}
 			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
 	// 영어단어 수정
-	
+
 	public void updateVoca(WordFormVO wordFormVo) {
-		
+
 		try {
 			boolean updateVoca = service.updateVoca(wordFormVo);
-			if(updateVoca == true) {
+			if (updateVoca == true) {
 				System.out.println("단어가 수정 되었습니다.");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
-		
+
 	}
-	
-	// 영어단어 삭제 
-	
+
+	// 영어단어 삭제
+
 	public void deleteVoca(WordFormVO wordFormVo) {
-		
+
 		try {
 			boolean deleteVoca = service.deleteVoca(wordFormVo);
-			if(deleteVoca == true) {
+			if (deleteVoca == true) {
 				System.out.println("단어가 삭제 되었습니다.");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
+
 	}
-	
-	
 
 }
--- Vocabulary/src/voca/view/RunningStartView.java
+++ Vocabulary/src/voca/view/RunningStartView.java
@@ -2,28 +2,30 @@ package voca.view;
 
 import voca.controller.WordController;
 import voca.dto.WordFormVO;
+import voca.exception.ValidationException;
 
 public class RunningStartView {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		WordController controller = WordController.getInstance();
 
-		System.out.println("모든 단어 검색");
+		System.out.println("모든 단어 검색 시작");
 		controller.allVoca();
 		System.out.println("=============================");
 		System.out.println();
 
-		System.out.println("한글 단어 검색 후 영어 출력 ");
+		System.out.println("한글 단어 검색 후 영어 출력 시작");
 		controller.searchVoca("양보하다");
 		System.out.println("=============================");
 		System.out.println();
 
-		System.out.println("영어 단어 추가");
+		System.out.println("영어 단어 추가 시작");
 		controller.addVoca(new WordFormVO("jejunum", "빈창자", 1));
 		controller.searchVoca("빈창자");
 		System.out.println("=============================");
 		System.out.println();
-
-//		controller.updateVoca(new WordFormVO(62 , "jejunum", "빈창자" , 1));
+		
+		
+		controller.updateVoca(new WordFormVO(62 , "jejunum", "빈창자" , 1));
 		controller.updateVoca(new WordFormVO(64, "cancer", "암", 1));
 		controller.searchVoca("암");
 		System.out.println("=============================");
