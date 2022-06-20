807df067ad00b6938941591333975b15a93b34aa
Uno0306 <82020768+Uno0306@users.noreply.github.com>
2022-06-20 09:49:56 +0900

    Exception handling update, delete
    Team leader makes it and I copy and paste the codes

--- Vocabulary/src/voca/model/MywordInterface.java
+++ Vocabulary/src/voca/model/MywordInterface.java
@@ -3,20 +3,13 @@ package voca.model;
 import java.sql.SQLException;
 
 import voca.exception.Notwordexception;
+import voca.dto.MywordDTO;
+import voca.dto.Word;
 
 public interface MywordInterface {
 	
 	public boolean mywordInsert(int wordId) throws SQLException, Notwordexception;
-//	public boolean mywordInsert(Word word) throws SQLException, Notwordexception;
 
+	public boolean mywordDelete(int myNumber) throws SQLException, Notwordexception;
 
-	public boolean mywordDelete(int wordId) throws SQLException;
-
-
-
-
-
-
-
-	
 }
